072253947f114abfb00129452ac38de607fb70bd
Arvind kumar <111298408+arvind9018@users.noreply.github.com>
2025-07-27 18:59:46 +0530

    Update settings.py

--- ChazeFashion/ChazeFashion/settings.py
+++ ChazeFashion/ChazeFashion/settings.py
@@ -117,13 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Optional: Only if you have static/ folder for custom CSS/JS
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# Required for collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
